a4adb396ffca8404cef0c20b58d64b4471ee7673
Matthew Serre <matthew.serre@gmail.com>
2019-06-13 21:03:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ class App < Sinatra::Base
   
   configure do
     enable :sessions
-    set :session_secret, ""
+    set :session_secret, "37vdS7mUcZAXq1!Sj8rvgKD6Mb#SC0Y#thVr*u67TaSwZgP!k#&KmRkFA@THFFwPWi8LUS1NDiu4YX31!PssdH1FutWDICU6uT@"
+  end
+  
+  get '/' do
+    erb :index
+  end
   
 end
